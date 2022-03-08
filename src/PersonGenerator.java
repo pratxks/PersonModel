65e119e4d0cbda2a7befd6df746a30013aaadec8
Pratik <pratxks@Pratiks-MacBook-Pro.local>
2022-03-08 00:06:47 -0500

    Initial commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.*;
+
+public class PersonGenerator
+{
+
+    static ArrayList<String> PersonRecordArray = new ArrayList<String>();
+
+    public static void InputPersonData()
+    {
+        boolean newPerson;
+
+        Scanner inStream = new Scanner(System.in);
+
+        String ID, firstName, lastName, title;
+        int yearOfBirth;
+
+        PersonRecordArray.clear();
+
+        do
+        {
+            ID = "";
+            firstName = "";
+            lastName = "";
+            title = "";
+            yearOfBirth = 0;
+            newPerson = false;
+
+            ID = SafeInput.getNonZeroLenString(inStream, "Enter Person ID (a string)");
+            firstName = SafeInput.getNonZeroLenString(inStream, "Enter Person first name");
+            lastName = SafeInput.getNonZeroLenString(inStream, "Enter Person last name");
+            title = SafeInput.getNonZeroLenString(inStream, "Enter Title of Person (a string like Mr., Mrs., Ms., Dr., etc.)");
+            yearOfBirth = SafeInput.getRangedInt(inStream, "Enter Person's Birth Year", 1, 2022);
+
+            String PersonRecord = "";
+            PersonRecord = String.format("%s, %s, %s, %s, %d", ID, firstName, lastName, title, yearOfBirth);
+            System.out.println(PersonRecord);
+
+            PersonRecordArray.add(PersonRecord);
+
+            if(!newPerson)
+            {
+                newPerson = SafeInput.getYNConfirm(inStream, "\n\nDo you want to input new person details (Y/N): ");
+            }
+
+        }while (newPerson);
+    }
+
+    public static void WritePersonDataToFile()//ArrayList<String> perRecArray)
+    {
+        // use the toolkit to get the current working directory of the IDE
+        // will create the file within the Netbeans project src folder
+        // (may need to adjust for other IDE)
+        // Not sure if the toolkit is thread safe...
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        System.out.println(file);
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String rec : PersonRecordArray)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+            }
+
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        InputPersonData();
+
+        for(int i=0; i<PersonRecordArray.size(); i++)
+        {
+            System.out.println(PersonRecordArray.get(i));
+        }
+
+        WritePersonDataToFile();
+    }
+}
---
+++ src/PersonReader.java
@@ -0,0 +1,126 @@
+import javax.swing.*;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class PersonReader
+{
+    static ArrayList<String> PersonRecordArray = new ArrayList<String>();
+
+    public static void ReadFileDataToArrayList()
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        PersonRecordArray.clear();
+
+        try
+        {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    PersonRecordArray.add(rec);
+                    line++;
+                    // echo to screen
+                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                }
+
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void DisplayPersonRecords()
+    {
+        //ID#           Firstname     Lastname       Title    YOB
+        String strHeader = String.format("%-11s","ID#");
+        strHeader += String.format("%-20s", "Firstname");
+        strHeader += String.format("%-20s", "Lastname");
+        strHeader += String.format("%-10s", "Title");
+        strHeader += String.format("%-10s", "YOB");
+
+        System.out.println(strHeader);
+        String strUnderLine = String.format("%71s", " ").replace(' ', '=');
+        System.out.println(strUnderLine);
+
+        for(String rec : PersonRecordArray)
+        {
+            String [] arrRecordFields = rec.split(",");
+            String strDisplayRec = "";
+
+            for(int i=0; i<arrRecordFields.length; i++)
+            {
+                String strRecField = arrRecordFields[i];
+
+                if(i == 0)
+                {
+                    strRecField = String.format("%8s", strRecField).replace(' ', '0');
+                    strRecField += String.format("%2s", " ");
+                }
+                if((i > 0) && (i < 3))
+                {
+                    strRecField = String.format("%-20s", strRecField);
+                }
+                if((i > 2) && (i < 5))
+                {
+                    strRecField = String.format("%-10s", strRecField);
+                }
+
+                strDisplayRec += strRecField;
+            }
+
+            System.out.println(strDisplayRec);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        ReadFileDataToArrayList();
+
+        DisplayPersonRecords();
+    }
+}
